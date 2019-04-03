deb1ebb96aa0fc8a412a8c04f12bf90b4b95e67d
Carden Z <ephemere@protonmail.com>
2019-04-03 19:12:15 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,11 @@
 class Transfer
   attr_reader :sender, :receiver
+  attr_accessor :pending
 
   def initialize(sender, receiver, ammount)
     @sender = sender
     @receiver = receiver
     @ammount = ammount
+    @status = 'pending'
   end
 end
