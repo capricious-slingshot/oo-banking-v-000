ccfb95ed02438fe85af23672fe05237106a3b20c
Carden Z <ephemere@protonmail.com>
2019-04-03 19:38:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,7 +27,7 @@ class Transfer
   end
 
   def reverse_transfer
-    if self.status = 'complete' && reciever.balance > amount
+    if self.status = 'complete' && receiver.balance > amount
       sender.balance += amount
       receiver.balance -= amount
     end
